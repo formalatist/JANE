89bbc3ac475e5a82064484fcbabf64a173093394
formalatist <formalatist@gmail.com>
2019-06-12 22:09:38 +0200

    changed default rom back to donkey-kong

--- oemulator/oemulator/main.cpp
+++ oemulator/oemulator/main.cpp
@@ -24,7 +24,7 @@ long getFileSize(FILE *file)
 
 
 int main(int argc, char** argv) {
-	const char* filePath = "C:\\Users\\Oivind\\Documents\\GitHub\\oemulator\\roms\\sprite_ram.nes";
+	const char* filePath = "C:\\Users\\Oivind\\Documents\\GitHub\\oemulator\\roms\\donkey-kong.nes";
 
 	unsigned char* fileBuffer;
 	FILE* file = NULL;
