7fffa44424655ac8b721314d2d36baee7d468bc5
formalatist <formalatist@gmail.com>
2020-04-24 03:45:40 +0200

    Getting close to a working nametable viewer

--- oemulator/oemulator/Core/Source Files/NametableView.cpp
+++ oemulator/oemulator/Core/Source Files/NametableView.cpp
@@ -7,37 +7,36 @@ NametableView::NametableView(const NES &nes_) : nes(nes_)
 		SDL_WINDOW_SHOWN);
 
 	nametableS = SDL_GetWindowSurface(window);
-	
-	draw();
 }
 
 void NametableView::draw()
 {
 	typedef unsigned char byte;
 	byte *mem = nes.ppuMemory->memory;
+	PPUMemory *ppuMem = nes.ppuMemory;
 	bool tableSide = (nes.ppu->CTRL & nes.ppu->CTRLBackgroundPattern) == nes.ppu->CTRLBackgroundPattern;
-
+	
 	int colors[4] = { 0x666666, 0xFFCCC5, 0x1412A7, 0xB53120 };
 	for (int PTX = 0; PTX < 2; PTX++) {
 		for (int PTY = 0; PTY < 2; PTY++) {
 			for (int x = 0; x < numColumns; x++) {
 				for (int y = 0; y < numRows; y++) {
-					int nametableEntry = mem[0x2000 + x + y*numColumns + PTX*numColumns*numRows + PTY*numColumns*numRows*2];
+					int nametableEntry = ppuMem->read(0x2000 + x + y*numColumns + PTX*(numColumns*numRows+64) + PTY*(numColumns*numRows+64)*2);
 					int attribEntryAddr = 0x1000 * tableSide + nametableEntry << 4;
 					//iterate over the pattern
 					for (int subY = 0; subY < 8; subY++) {
-						byte patternRowTop = mem[attribEntryAddr + subY];
-						byte patternRowBot = mem[attribEntryAddr + subY + 8];
+						byte patternRowTop = ppuMem->read(attribEntryAddr + subY);
+						byte patternRowBot = ppuMem->read(attribEntryAddr + subY + 8);
 						for (int subX = 0; subX < 8; subX++) {
-							byte pixelValue = ((patternRowBot >> (subX - 1)) & 0b10)
-								| ((patternRowTop >> (subX)) & 0b1);
+							byte pixelValue = ((patternRowTop >> (subX - 1)) & 0b10)
+								| ((patternRowBot >> (subX)) & 0b1);
 							int colorValue = colors[pixelValue];
 
 							Uint8 *targetPixel = (Uint8*)nametableS->pixels
 								+ (y * 8 + subY)*nametableS->pitch
 								+ (x * 8 + subX + PTX*256 + PTY*256*240*2) * 4;
 							//std::cout << "X : " << x << " subX : " << subX << " Y : " << y << " subY : " << subY << std::endl;
-							*(Uint32*)targetPixel = colorValue;
+							*(Uint32*)targetPixel = PaletteLookup[ppuMem->read(pixelValue + 0x3F00)];
 						}
 					}
 				}
